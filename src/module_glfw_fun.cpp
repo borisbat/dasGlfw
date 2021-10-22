6c80d932e496f79fd6874dc64258a22516955528
Boris Batkin <bbatkin@gmail.com>
2021-10-22 00:01:52 -0700

    wip - hazardous goo

--- src/module_glfw_fun.cpp
+++ src/module_glfw_fun.cpp
@@ -12,7 +12,26 @@ using namespace das;
 #include "module_glfw.h"
 #endif
 
+#ifdef _MSC_VER
+
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
+namespace das {
+    void * DAS_glfwGetWin32Window(GLFWwindow* window) {
+        auto wnd = glfwGetWin32Window(window);
+        return (void *) wnd;
+    }
+}
+
+#endif
+
 void Module_glfw::initFunctions ( ) {
+#ifdef _MSC_VER
+    addExtern<DAS_BIND_FUN(DAS_glfwGetWin32Window)>(*this, lib, "glfwGetWin32Window",SideEffects::worstDefault, "DAS_glfwGetWin32Window")
+	    ->args({"window"});
+#endif
+
 #if USE_GENERATED
 #include "module_glfw.inc"
 #endif
