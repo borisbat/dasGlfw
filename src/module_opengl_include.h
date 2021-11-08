bbd0c69f2c28e4ee24ac9225eb799498b8c453bc
Boris Batkin <bbatkin@gmail.com>
2021-11-07 21:02:12 -0800

    bye-bye hard-bound opengl

--- src/module_opengl_include.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "opengl_headers.h"
-
-MAKE_EXTERNAL_TYPE_FACTORY(__GLsync,__GLsync);
-MAKE_EXTERNAL_TYPE_FACTORY(_cl_context,_cl_context);
-MAKE_EXTERNAL_TYPE_FACTORY(_cl_event,_cl_event);
--- src/opengl_headers.h
+++
@@ -1,7 +0,0 @@
-#define APIENTRY	__stdcall
-
-#ifndef NO_GL_GLEXT_PROTOTYPES
-#define GL_GLEXT_PROTOTYPES	1
-#endif
-
-#include "OpenGL/glcorearb.h"
